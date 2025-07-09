c308c710f93442d20196ba6c24641508e2a5aa66
Travis CI <yadusurya666@gmail.com>
2025-07-09 23:59:57 +0530

    GoogleAdsense code placed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({
                 <Providers>
                     {children}
                 </Providers>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9427726908265759"
+     crossOrigin="anonymous"></script>
             </body>
         </html>
     );
